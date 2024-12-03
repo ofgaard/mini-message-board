9ce6e8f498553b28438ef84605e01ae2dad4fda7
Oliver <oliverfruergaard@gmail.com>
2024-12-03 13:14:38 +0100

    remove indexcontroller (duplicate)

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
-const { getMessages } = require("../controllers/indexController.js");
-const { addMessage, getForm } = require("../controllers/formController.js");
+const {
+  addMessage,
+  getForm,
+  getMessages,
+} = require("../controllers/formController.js");
 
 const indexRouter = Router();
 
 indexRouter.get("/", getMessages);
 indexRouter.get("/new", getForm);
 indexRouter.post("/new", addMessage);
+
 module.exports = indexRouter;
